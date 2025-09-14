d7069cf20d53dd292410247c02d600d6a8597897
Sandhya-ad <adhikar2@ualberta.ca>
2025-09-14 10:39:08 -0600

    Some fixes in application

--- applications/models.py
+++ applications/models.py
@@ -16,8 +16,8 @@ class JobApplication(models.Model):
     company_name = models.CharField(max_length=255)
     position = models.CharField(max_length=255)
     date_applied = models.DateField(auto_now_add=True)
+    notes = models.TextField(blank=True, default="")
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="applied")
-    # notes = models.TextField(blank=True)
     follow_up_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
